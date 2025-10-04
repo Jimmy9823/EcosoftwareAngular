d094b47aee760227532e1422608392e819e9bfb4
Paula Sepulveda <paula06sepulveda@gmail.com>
2025-10-04 01:40:53 -0500

    error 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,8 @@ import { CardsSolicitud } from './solcitudes/cards-solicitud/cards-solicitud';
 import { CardARSolicitud } from './solcitudes/card-a-r-solicitud/card-a-r-solicitud';
 import { FormRegistro } from './solcitudes/form-registro/form-registro';
 import { EditarUsuario } from './usuario/editar-usuario/editar-usuario';
+import { Error} from './pages/error/error';
+
 
 export const routes: Routes = [
     {path: '', component: Inicio},
@@ -26,6 +28,6 @@ export const routes: Routes = [
     {path: 'card-re-acpt-solicitud', component:CardARSolicitud},
     {path: 'form-solicitud', component:FormRegistro},
 
-    {path: '**', redirectTo: ''} //cualquier otra ruta redirecciona a inicio
+  { path: '**', component: Error }
 ];
 
---
+++ src/app/pages/error/error.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Inicio } from '../../inicio/inicio';
+
+@Component({
+  selector: 'app-error',
+  imports: [],
+  templateUrl: './error.html',
+  styleUrl: './error.css'
+})
+export class Error {
+
+}
