0672fba0796711336bd25ae3a312d45fb55eb0f0
Jaime <jaiandroaber@gmail.com>
2025-10-19 01:46:56 -0500

    Vista solicitudes

--- src/app/usuario/administrador/vista-usuarios/vista-usuarios.ts
+++ src/app/usuario/administrador/vista-usuarios/vista-usuarios.ts
@@ -1,16 +1,47 @@
 import { Component } from '@angular/core';
 import { COMPARTIR_IMPORTS } from '../../../ImpCondYForms/imports';
-
+import { Tabla } from '../../../shared/tabla/tabla';
+import { UsuarioService } from '../../usuario_services/usuario.service';
+import {UsuarioModel} from '../../../usuario/usuario_models/usuario'
 @Component({
   selector: 'app-vista-usuarios',
-  imports: [COMPARTIR_IMPORTS],
+  imports: [COMPARTIR_IMPORTS, Tabla ],
   templateUrl: './vista-usuarios.html',
   styleUrl: './vista-usuarios.css'
 })
 export class VistaUsuarios {
-  usuarios = [
-    { nombre: 'Juan', rol: 'Ciudadano' },
-    { nombre: 'María', rol: 'Reciclador' },
-    { nombre: 'Pedro', rol: 'Empresa' }
+
+  cargando:boolean = false
+  usuarios: UsuarioModel[]=[]
+  error = ''
+  constructor(private usuarioService: UsuarioService) {}
+
+  
+    ngOnInit(): void {
+      this.cargarUsuarios()
+    }
+  columns = [
+    { key: 'idUsuario', label: 'Id' },
+    { key: 'nombre', label: 'Nombre' },
+    { key: 'correo', label: 'Correo Electrónico' },
+    { key: 'telefono', label: 'Telefono' }
   ]
+
+  data: any []=[]
+
+  cargarUsuarios(): void {
+    this.cargando = true
+    this.usuarioService.listar().subscribe({
+      next: (data) => {
+        this.usuarios = data
+        this.cargando = false
+      },
+      error: (err) => {
+        console.error('Error al cargar usuarios:', err)
+        this.error = 'Error al cargar usuarios'
+        this.cargando = false
+        setTimeout(() => this.error = '', 2500);
+      }
+    })
+  }
 }
