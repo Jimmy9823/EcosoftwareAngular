1a82c8da96a999f4d98020c9f8b5f7e6270226c4
Jaime <jaiandroaber@gmail.com>
2025-11-15 00:28:31 -0500

    Proceso mapa

---
+++ src/app/Services/puntos-reciclaje.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PuntoReciclaje } from '../Models/puntos-reciclaje.model';
+
+@Injectable({ providedIn: 'root' })
+export class PuntosService {
+
+  private url = 'http://localhost/Eco_Software-/api/puntos.php';
+
+  constructor(private http: HttpClient) {}
+
+  obtenerTodos(): Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  obtenerPorId(id: number): Observable<any> {
+    return this.http.get(`${this.url}?id=${id}`);
+  }
+
+  crear(punto: PuntoReciclaje): Observable<any> {
+    return this.http.post(this.url, punto);
+  }
+
+  actualizar(id: number, punto: PuntoReciclaje): Observable<any> {
+    return this.http.put(`${this.url}?id=${id}`, punto);
+  }
+
+  eliminar(id: number): Observable<any> {
+    return this.http.delete(`${this.url}?id=${id}`);
+  }
+}
